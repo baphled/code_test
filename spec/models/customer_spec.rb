a964ae3d8cc762f89f014d86ad3aa2e7f53d7d89
Yomi Colledge <baphled@boodah.net>
2018-03-07 22:19:08 +0000

    Update VCRs

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -22,59 +22,6 @@ RSpec.describe Customer do
     )
   end
 
-  describe '#new' do
-    subject do 
-      described_class.new(
-        access_token: access_token,
-        pGUID: pGUID,
-        pAccName: pAccName,
-        pPartner: pPartner,
-        name: name,
-        business_name: business_name,
-        telephone_number: telephone_number,
-        email: email,
-        contact_name: contact_name,
-        notes: notes,
-        reference: reference,
-      )
-    end
-
-    let(:business_name) { 'Some Business' }
-    let(:telephone_number) { '0203 333 3333' }
-    let(:email) { 'y@me.com' }
-    let(:contact_name) { 'Joe Bloggs' }
-    let(:notes) { 'My notes' }
-    let(:reference) { 'My reference' }
-
-    it 'has a name' do
-      expect(subject.name).to eql(name)
-    end
-
-    it 'has a business name' do
-      expect(subject.business_name).to eql(business_name)
-    end
-
-    it 'has a telephone number' do
-      expect(subject.telephone_number).to eql(telephone_number)
-    end
-
-    it 'has a email' do
-      expect(subject.email).to eql(email)
-    end
-
-    it 'has a contact name' do
-      expect(subject.contact_name).to eql(contact_name)
-    end
-
-    it 'has a reference' do
-      expect(subject.reference).to eql(reference)
-    end
-
-    it 'has a notes' do
-      expect(subject.notes).to eql(notes)
-    end
-  end
-
   describe '#post' do
     let(:email) { 'y@me.com' }
     let(:contact_name) { 'Joe Bloggs' }
