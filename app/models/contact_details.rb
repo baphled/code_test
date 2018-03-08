f3d8604338fd59f4d93af188b0d3ea094c7e72e8
Yomi Colledge <baphled@boodah.net>
2018-03-08 01:59:22 +0000

    Add some notes

--- app/models/contact_details.rb
+++ app/models/contact_details.rb
@@ -1,11 +1,21 @@
 class ContactDetails
   include ActiveModel::Model
 
+  # NOTE: Only real reason to use these validations is for simple_form.
+  #
+  # Duplicating validation is usually a code smell
+  #
+  validates_presence_of :name
+  validates_presence_of :business_name
+  validates_presence_of :telephone_number
+
+  # Mandatory user input
   attr_accessor :access_token
   attr_accessor :pGUID
   attr_accessor :pPartner
   attr_accessor :pAccName
 
+  # Optional user input
   attr_accessor :name
   attr_accessor :business_name
   attr_accessor :telephone_number
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,21 @@ class Customer < ActiveResource::Base
 
   private
 
+  #
+  # NOTE: API end-point is a non-standard REST API
+  # 
+  # POST should not accept query parameters and should expect parameter body instead.
+  #
+  # As this is not the case we overload the below method so that we can make the correct call to our external API
+  #
+  # "POST /api/v1/create? ..."
+  #
+  # A REST API would allow something like "POST /api/v1/leads.json"
+  #
+  # It seems that the API is not idempotent either, so duplicate entries would be an issue. In these cases we would
+  # expect a HTTP status code of 409 though so far I've been unable to replicate this.
+  #
+
   def custom_method_new_element_url(method_name, options = {})
     "#{self.class.prefix(prefix_options)}#{method_name}#{self.class.__send__(:query_string, options)}"
   end
